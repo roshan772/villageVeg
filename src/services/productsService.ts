61d4dda1e9fe166cdaea61f7d385761e04900157
roshan772 <krishroishanroshan1221@gmail.com>
2026-01-24 10:51:43 +0530

    get product method implemented

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase/auth";
-import { collection, doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, orderBy, query } from "firebase/firestore";
 import { db } from "./firebase";
+import { Product } from "../types/product";
 
 const auth = getAuth();
 const productCollection = collection(db, "products");
@@ -13,4 +14,25 @@ async function isAdmin(): Promise<boolean> {
   const snap = await getDoc(userRef); //When you call getDoc(userRef), Firebase goes to the "users" collection and finds the document for that specific UID. It returns the snap object.
   if (!snap.exists()) return false; //This is a built-in method. Because you are searching for a user by their ID, there is a chance that user doesn't actually exist in your database yet.
   return snap.data().role === "admin"; //Reading the letter	Pulls out the actual information (the role).
+}
+
+//Get products
+export const getProducts = async ():Promise<Product[]> =>{
+    const q = query(productCollection, orderBy("createdAt","desc"));
+    const snapshot = await getDocs(q)
+    return snapshot.docs.map((d)=>{
+        const data = d.data()
+        return {
+          id: d.id,
+          name: data.name as string,
+          price: data.price as number,
+          unit: data.unit as string,
+          stock: data.stock as number,
+          category: data.category as string,
+          description: (data.description as string) || "",
+          imageUrl: (data.imageUrl as string) || "",
+          createdAt: data.createdAt as string,
+          createdBy: data.createdBy as string,
+        };
+    })
 }
